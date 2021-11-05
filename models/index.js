93fd7a86338701b565e6b06d693e9a2bddaa28c9
Breania Harvey <harvey.breania@gmail.com>
2021-11-05 12:06:37 -0500

    table relationships added to models/index

--- models/index.js
+++ models/index.js
@@ -4,13 +4,37 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category: a product can only belong to one category
+// Product belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products: category can have many products
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag): products can have many tags
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'items',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag): tags can belong to many products
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
